d3242ba563d101d7606611c08181420315f77569
jjaanniikk <janik.kaempfer@gmail.com>
2023-10-03 10:34:56 +0200

    Implement angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WorkComponent } from './work/work.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/aboutme', pathMatch: 'full' },
+  { path: 'aboutme', component: AboutmeComponent },
+  { path: 'work', component: WorkComponent },
+  { path: '**', redirectTo: '/aboutme', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { WorkComponent } from './work/work.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutmeComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
