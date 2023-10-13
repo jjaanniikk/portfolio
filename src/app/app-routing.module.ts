c6ea709cf6c1f0753ebee377c3ab8dd8044df172
jjaanniikk <janik.kaempfer@gmail.com>
2023-10-13 15:45:05 +0200

    Update "about" site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,11 @@ import { AboutmeComponent } from './aboutme/aboutme.component';
 import { LifeComponent } from './life/life.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/aboutme', pathMatch: 'full' },
-  { path: 'aboutme', component: AboutmeComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: AboutmeComponent },
   { path: 'work', component: WorkComponent },
   { path: 'life', component: LifeComponent },
-  { path: '**', redirectTo: '/aboutme', pathMatch: 'full' }
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
 ];
 
 @NgModule({
